af99793bf9872d11cdd2b60cb52703b77a4a32ab
teloi <99101154@qq.com>
2022-03-14 11:09:19 +0800

    Init

---
+++ scripts/list.js
@@ -0,0 +1,63 @@
+vueApp.examples = [
+  {
+    "title": "Html",
+    "list": [
+      {
+        "title": "滚动文字",
+        "url": "./page-html/html-scroll-text.html"
+      },
+    ]
+  },
+  {
+    "title": "Css",
+    "list": [
+      {
+        "title": "清明节专用-彩虹变黑白",
+        "url": "./page-css/css-gray-background.html"
+      },
+      {
+        "title": "鼠标悬浮蓝底",
+        "url": "./page-css/css-mouse-hover-blue.html"
+      },
+      {
+        "title": "凹陷风格输入框",
+        "url": "./page-css/css-input-shadow-style.html"
+      }
+    ]
+  },
+  {
+    "title": "Js",
+    "list": [
+      {
+        "title": "获取当前用户浏览器信息",
+        "url": "./page-js/js-user-agent-info.html"
+      },
+      {
+        "title": "获取GPU信息",
+        "url": "./page-js/js-gpu-info.html"
+      },
+      {
+        "title": "禁止缓存js-随机时间戳",
+        "url": "./page-js/js-disable-cache.html"
+      }
+    ]
+  },
+  {
+    "title": "Algorithm",
+    "list": [
+      {
+        "title": "归并排序-1",
+        "url": "./page-algorithm/algorithm-mergesort.html"
+      },
+      {
+        "title": "两个栈实现队列",
+        "url": "./page-algorithm/algorithm-two-stack-queue.html"
+      }
+    ]
+  },
+  {
+    "title": "Demos",
+    "list": [
+    ]
+  }
+]
